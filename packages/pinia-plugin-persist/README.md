9d06707f96e00c795babad760ea8b0d0fa772f2d
Nick Lowe <nicklowe@snickbit.com>
2022-05-02 17:02:05 -0400

    init(pinia-plugin-persist): first commit

---
+++ packages/pinia-plugin-persist/README.md
@@ -0,0 +1,29 @@
+# @snickbit/pinia-plugin-persist
+
+<!--START_SECTION:readmes-description-->
+
+Pinia plugin to persist state using LocalForage
+
+<!--END_SECTION:readmes-description-->
+
+## Installation
+
+```bash
+yarn add @snickbit/pinia-plugin-persist
+```
+
+```bash
+npm add @snickbit/pinia-plugin-persist
+```
+
+## Documentation
+
+### [API Docs](https://github.com/snickbit/pinia/blob/main/packages/pinia-plugin-persist/README.md)
+
+### [CHANGELOG](https://github.com/snickbit/pinia/blob/main/packages/pinia-plugin-persist/CHANGELOG.md)
+
+## License
+
+Copyright (c) 2022 - **Nicholas Lowe** aka **Snickbit**
+
+[MIT License](https://github.com/snickbit/pinia/blob/main/LICENSE)
---
+++ packages/pinia-plugin-persist/src/index.ts
@@ -0,0 +1,50 @@
+import {isEmpty, isObject, objectExcept, objectOnly} from '@snickbit/utilities'
+import localforage from 'localforage'
+import {Store} from 'pinia'
+
+export interface PersistenceObject {
+	storageKey?: string
+	include?: string[]
+	ignore?: string[]
+}
+
+export type PersistenceArray = string[]
+
+export interface PersistenceOptions {
+	persist: PersistenceObject | PersistenceArray | boolean
+}
+
+export interface PersistenceParams {
+	options: PersistenceOptions
+	store: Store
+}
+
+export default async function ({options, store}: PersistenceParams): Promise<void> {
+	if (options.persist) {
+		if (options.persist === true) options.persist = {}
+		else if (Array.isArray(options.persist)) options.persist = {include: options.persist}
+		else if (!isObject(options.persist)) throw new Error('Invalid persist options')
+
+		const config = {
+			storageKey: store.$id,
+			include: [],
+			ignore: [],
+			...options.persist
+		}
+
+		const updateStorage = async () => {
+			let state = JSON.parse(JSON.stringify(store.$state))
+			if (!isEmpty(config.include)) {
+				state = objectOnly(state, config.include)
+			}
+			if (!isEmpty(config.ignore)) {
+				state = objectExcept(state, config.ignore)
+			}
+			await localforage.setItem(config.storageKey, state)
+		}
+
+		localforage.getItem(config.storageKey).then(store.$patch)
+		await updateStorage()
+		store.$subscribe(() => updateStorage())
+	}
+}
