f12958642fb9d15a6cdf95e0048a3ff1ff3f8e3d
Nick Lowe <nicklowe@snickbit.com>
2022-05-02 17:01:28 -0400

    init(pinia-plugin-loading): first commit

---
+++ packages/pinia-plugin-loading/README.md
@@ -0,0 +1,29 @@
+# @snickbit/pinia-plugin-loading
+
+<!--START_SECTION:readmes-description-->
+
+Pinia plugin to track loading state of all asynchronous actions
+
+<!--END_SECTION:readmes-description-->
+
+## Installation
+
+```bash
+yarn add @snickbit/pinia-plugin-loading
+```
+
+```bash
+npm add @snickbit/pinia-plugin-loading
+```
+
+## Documentation
+
+### [API Docs](https://github.com/snickbit/pinia/blob/main/packages/pinia-plugin-loading/README.md)
+
+### [CHANGELOG](https://github.com/snickbit/pinia/blob/main/packages/pinia-plugin-loading/CHANGELOG.md)
+
+## License
+
+Copyright (c) 2022 - **Nicholas Lowe** aka **Snickbit**
+
+[MIT License](https://github.com/snickbit/pinia/blob/main/LICENSE)
---
+++ packages/pinia-plugin-loading/src/index.ts
@@ -0,0 +1,28 @@
+import {isAsyncFunction} from '@snickbit/utilities'
+import {ref} from 'vue'
+
+export type AsyncFunction<T> = (...args: any[]) => Promise<T>
+
+export default function ({store}) {
+	const _loading = ref([])
+
+	for (let [key, prop] of Object.entries(store)) {
+		if (isAsyncFunction(prop)) {
+			store[key] = async function (...args) {
+				const promise = (prop as AsyncFunction<any>).call(this, ...args)
+				_loading.value.push(promise)
+				return await promise.finally(() => _loading.value.splice(_loading.value.indexOf(promise), 1))
+			}
+		}
+	}
+
+	const isLoading = () => _loading.value.length > 0
+
+	return {
+		async ready() {
+			await Promise.all(_loading.value)
+		},
+		_loading,
+		isLoading
+	}
+}
