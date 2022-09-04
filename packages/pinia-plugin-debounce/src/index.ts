ef2bb5840fcaef83959cfe3ed9b6846bac7072ae
Snickbit <snickbit@users.noreply.github.com>
2022-09-03 22:08:37 -0400

    fix: use shared eslint config (#17)

--- packages/pinia-plugin-debounce/src/index.ts
+++ packages/pinia-plugin-debounce/src/index.ts
@@ -2,11 +2,12 @@ import debounce from 'debounce'
 
 export default function({options, store}) {
 	if (options.debounce) {
-		// we are overriding the actions with new ones
-		return Object.keys(options.debounce).reduce((debouncedActions, action) => {
-			debouncedActions[action] = debounce(store[action],
-				options.debounce[action])
-			return debouncedActions
-		}, {})
+		const debouncedActions = {}
+
+		for (const action in options.debounce) {
+			debouncedActions[action] = debounce(store[action], options.debounce[action])
+		}
+
+		return debouncedActions
 	}
 }
